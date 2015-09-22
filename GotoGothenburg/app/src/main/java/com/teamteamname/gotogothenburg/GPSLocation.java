89643ca047764ea773d135f043bc8281d51bb34a
Kakan-IT14 <jesper.olsson191@gmail.com>
2015-09-22 08:29:31 +0200

    added class GPSLocation

---
+++ GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/GPSLocation.java
@@ -0,0 +1,24 @@
+package com.teamteamname.gotogothenburg;
+
+/**
+ * Created by kakan on 2015-09-22.
+ * A class defining a co-ordinate.
+ */
+public class GPSLocation {
+
+    private float latitude;
+    private float longitude;
+
+    public GPSLocation(final float latitude, final float longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public float getLatitude() {
+        return this.latitude;
+    }
+
+    public float getLongitude() {
+        return this.longitude;
+    }
+}
