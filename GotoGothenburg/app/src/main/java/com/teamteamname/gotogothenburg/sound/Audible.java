2d1c343bf714dac38da3110484706fdf3277e7d1
Kakan-IT14 <jesper.olsson191@gmail.com>
2015-09-22 09:18:23 +0200

    created interface Audible

---
+++ GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/sound/Audible.java
@@ -0,0 +1,10 @@
+package com.teamteamname.gotogothenburg.sound;
+
+import android.content.Context;
+
+/**
+ * Created by kakan on 2015-09-22.
+ */
+public interface Audible {
+    public void playSound(Context context);
+}
