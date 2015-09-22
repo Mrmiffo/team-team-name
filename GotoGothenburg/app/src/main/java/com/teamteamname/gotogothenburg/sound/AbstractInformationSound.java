7427c923324c0cc5d2197b5f137a0db6422c1791
Kakan-IT14 <jesper.olsson191@gmail.com>
2015-09-22 11:03:36 +0200

    added method isInArea()

--- GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/sound/AbstractInformationSound.java
+++ GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/sound/AbstractInformationSound.java
@@ -5,14 +5,17 @@ import android.media.MediaPlayer;
 
 import com.teamteamname.gotogothenburg.GPSLocation;
 
+import java.io.Serializable;
+
 /**
  * Created by kakan on 2015-09-22.
  */
-public abstract class AbstractInformationSound implements Audible {
+public abstract class AbstractInformationSound implements Audible, Serializable {
 
     private GPSLocation location;
     private float radius;
     private int resID;
+
 
     public AbstractInformationSound(GPSLocation location, float radius, int resID) {
         this.location = location.clone();
@@ -24,5 +27,15 @@ public abstract class AbstractInformationSound implements Audible {
     public void playSound(Context context) {
         MediaPlayer mediaPlayer = MediaPlayer.create(context, resID);
         mediaPlayer.start();
+    }
+
+    /**
+     * Evaluates whether or not a given coordinate is enclosed in the area.
+     * @param coordinate the coordinate to evaluate.
+     * @return true if the coordinate is in the area, false otherwise.
+     */
+    public boolean isInArea(GPSLocation coordinate) {
+        // (x - x_center)² + (y - y_center)² < radius²
+        return Math.pow(coordinate.getLatitude()-location.getLatitude(), 2) + Math.pow(coordinate.getLongitude()-location.getLongitude(), 2) < Math.pow(radius, 2);
     }
 }
