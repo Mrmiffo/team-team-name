d484bc764d117f53b13b729e2eaa414cdf5698e8
Kakan-IT14 <jesper.olsson191@gmail.com>
2015-09-24 14:42:41 +0200

    name update

--- GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/GPSListener.java
+++ GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/GPSListener.java
@@ -1,8 +1,9 @@
 package com.teamteamname.gotogothenburg;
 
 /**
+ * An interface for all observers interested in change of GPS location.
  * Created by kakan on 2015-09-22.
  */
 public interface GPSListener {
-    void updatedLocation(GPSLocation newLocation);
+    void updatedLocation(GPSCoord newLocation);
 }
