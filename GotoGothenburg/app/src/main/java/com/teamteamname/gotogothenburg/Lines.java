d73f0f7ff7e13e31238468e8805fd233761ff44a
Kakan-IT14 <jesper.olsson191@gmail.com>
2015-09-25 11:54:08 +0200

    added enum for lines (e.g. bus 55, tram 8)

---
+++ GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/Lines.java
@@ -0,0 +1,10 @@
+package com.teamteamname.gotogothenburg;
+
+/**
+ * An enum containing all valid lines (e.g. bus 55, tram 8).
+ * Created by kakan on 2015-09-25.
+ */
+public enum Lines {
+    //Note that only line bus 55 is included in the prototype.
+    BUS_55;
+}
