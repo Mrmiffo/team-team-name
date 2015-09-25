d9392eb34f9986778a190bee7e1df056ff3c45e7
Kakan-IT14 <jesper.olsson191@gmail.com>
2015-09-25 11:48:45 +0200

    created enum for all stops

---
+++ GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/Stops.java
@@ -0,0 +1,23 @@
+package com.teamteamname.gotogothenburg;
+
+/**
+ * An enum containing all valid stops for buses, tram etc.
+ * Created by kakan on 2015-09-25.
+ */
+public enum Stops {
+    //Note that only stops located along line 55 are included in the prototype.
+    Sven_Hultings_Gata,
+    Chalmersplatsen,
+    Kapellplatsen,
+    Götaplatsen,
+    Valand,
+    Kungsportsplatsen,
+    Brunnsparken,
+    Lilla_Bommen,
+    Frihamnsporten,
+    Pumpgatan,
+    Regnbågsgatan,
+    Lindholmen,
+    Teknikgatan,
+    Lindholmsplatsen;
+}
