b128e111c88ac35c8fc4afba2e220faca38ad43d
Olodus <olof.dusterdieck@gmail.com>
2015-09-28 11:27:16 +0200

    Added a test class for ElectricityAPI.

---
+++ GotoGothenburg/app/src/androidTest/java/com/teamteamname/gotogothenburg/API/ElectricityAPITest.java
@@ -0,0 +1,17 @@
+package com.teamteamname.gotogothenburg.API;
+
+import android.test.ActivityUnitTestCase;
+
+/**
+ * Created by Olof on 28/09/2015.
+ */
+public class ElectricityAPITest extends ActivityUnitTestCase {
+    private ElectricityAPI api;
+
+    public ElectricityAPITest(Class activityClass) { super(activityClass); }
+
+    public void setUp() throws Exception{
+        super.setUp();
+
+    }
+}
