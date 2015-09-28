7b53a8fa914e6743674425b257b0b31983f3070f
Kakan-IT14 <jesper.olsson191@gmail.com>
2015-09-28 08:45:21 +0200

    the Lines enum now holds information about stops and nearby POIs

---
+++ GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/PointOfInterest.java
@@ -0,0 +1,24 @@
+package com.teamteamname.gotogothenburg;
+
+/**
+ * Created by kakan on 2015-09-28.
+ */
+public enum PointOfInterest {
+    Johanneberg_Science_Park,
+    Chalmers,
+    Landala,
+    Poseidon,
+    Göteborgs_konstmuseum,
+    Stadsteatern,
+    Göteborgs_konserthus,
+    Kopparmärra,
+    Såningskvinnan,
+    Nordstan,
+    Arkaden,
+    Domstolen,
+    Läppstiftet,
+    Eventyta,
+    Kuggen,
+    Lindholmen_Science_Park,
+    Inomhushållplatsen;
+    }
