92cf49feaf84e9e19ef10580db9f98720197a471
Olodus <olof.dusterdieck@gmail.com>
2015-09-28 13:43:08 +0200

    Made ElectricityAPI singleton.

--- GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/api/ElectricityAPI.java
+++ GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/api/ElectricityAPI.java
@@ -6,16 +6,12 @@ import android.util.Base64;
 import android.util.Log;
 
 import com.android.volley.AuthFailureError;
-import com.android.volley.Cache;
-import com.android.volley.Network;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.BasicNetwork;
-import com.android.volley.toolbox.DiskBasedCache;
-import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.teamteamname.gotogothenburg.GPSCoord;
+import com.teamteamname.gotogothenburg.map.Bus;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -30,67 +26,109 @@ import java.util.Map;
 public class ElectricityAPI implements IElectricityAPI{
 
     private RequestQueue queue;
+    private static ElectricityAPI instance;
 
     private String username = "grp27";
     private String password = "BCcRl-8UlI";
 
-    private String baseUrl = "https://ece01.ericsson.net:4443/ecity";
-    private String dgw = "dgw=";
-    private String sensorSpec = "sensorSpec=";
-    private String resourceSpec = "resourceSpec=";
-    private String t1 = "t1=";
-    private String t2 = "t2=";
-
-    // In cases we have a common queue
-    public ElectricityAPI(RequestQueue queue){
+    private ElectricityAPI(RequestQueue queue){
         this.queue = queue;
     }
 
-    // In case every API has it's own queue
-    public ElectricityAPI(Context context){
-        Cache cache = new DiskBasedCache(context.getCacheDir(),1024*1024);
+    public static void init(Context context, RequestQueue queue){
+        if(instance == null){
+            instance = new ElectricityAPI(queue);
 
-        Network network = new BasicNetwork(new HurlStack());
-
-        queue = new RequestQueue(cache,network);
-
-        queue.start();
+            //TODO: Fetch api username and password from values.strings
+        }
     }
 
-    public void getBusLocation(String dgw, ElectricityHandler callback){
+    public static ElectricityAPI getInstance(){
+        return instance;
+    }
+
+    public void getBusLocation(Bus bus, ElectricityHandler callback){
         //Requests data since 5 sec earlier.
         long t2 = System.currentTimeMillis();
         long t1 = t2 - (1000 * 5);
 
         // Builds the URI
-        StringBuilder sb = new StringBuilder();
-        sb.append(baseUrl);
-        sb.append("?");
-        sb.append(this.dgw);
-        sb.append(dgw);
-        sb.append("&");
-        sb.append(sensorSpec);
-        sb.append("Ericsson$GPS");
-        sb.append("&");
-        sb.append(this.t1);
-        sb.append(t1);
-        sb.append("&");
-        sb.append(this.t2);
-        sb.append(t2);
-        String uri = sb.toString();
+        String uri = buildURI(bus.getDgw(),"Ericsson$GPS2",null,t1,t2);
 
         Log.i("URI", uri);
 
         // Adds a parser for handling the JSONArray and gives it a class to inform when done.
         GPSCoordParser parser = new GPSCoordParser(callback);
 
-        GetRequestWithBAuth request = new GetRequestWithBAuth(uri,parser,parser);
+        RequestWithBAuth request = new RequestWithBAuth(uri,parser,parser);
         queue.add(request);
 
     }
 
-    private String buildURI(){
-        return null;
+    @Override
+    public void getNextStop(Bus bus, ElectricityHandler callback) {
+        //Requests data since 10 sec earlier.
+        long t2 = System.currentTimeMillis();
+        long t1 = t2 - (1000 * 10);
+
+        // Builds the URI
+        String uri = buildURI(bus.getDgw(),"Ericsson$Next_Stop",null,t1,t2);
+
+        Log.i("URI", uri);
+
+        // Adds a parser for handling the JSONArray and gives it a class to inform when done.
+        NextStopParser parser = new NextStopParser(callback);
+
+        RequestWithBAuth request = new RequestWithBAuth(uri,parser,parser);
+        queue.add(request);
+    }
+
+    @Override
+    public void getAmbientTemperature(Bus bus, ElectricityHandler callback) {
+
+    }
+
+    @Override
+    public void getCabinTemperature(Bus bus, ElectricityHandler callback) {
+
+    }
+
+    @Override
+    public void getStopPressed(Bus bus, ElectricityHandler callback) {
+
+    }
+
+    @Override
+    public void getNbrOfWifiUsers(Bus bus, ElectricityHandler callback) {
+
+    }
+
+    // sensorSpec and resourceSpec can't be given at the same time.
+    private String buildURI(String dgw, String sensorSpec, String resourceSpec, long t1, long t2){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("https://ece01.ericsson.net:4443/ecity?");
+        if(dgw != null){
+            sb.append("dgw=");
+            sb.append(dgw);
+            sb.append("&");
+        }
+        if(sensorSpec != null){
+            sb.append("sensorSpec=");
+            sb.append(sensorSpec);
+            sb.append("&");
+        }else if(resourceSpec != null){
+            sb.append("resourceSpec=");
+            sb.append(resourceSpec);
+            sb.append("&");
+        }
+        sb.append("t1=");
+        sb.append(t1);
+        sb.append("&");
+        sb.append("t2=");
+        sb.append(t2);
+
+        return sb.toString();
     }
 
     private Map<String,String> createBasicAuth(String username, String password){
@@ -106,9 +144,9 @@ public class ElectricityAPI implements IElectricityAPI{
     }
 
     // A JSONArray-GET-request with our Basic Authorization as header.
-    private class GetRequestWithBAuth extends JsonArrayRequest{
+    private class RequestWithBAuth extends JsonArrayRequest{
 
-        public GetRequestWithBAuth(String uri, Response.Listener<JSONArray> listener, Response.ErrorListener errorListener){
+        public RequestWithBAuth(String uri, Response.Listener<JSONArray> listener, Response.ErrorListener errorListener){
             super(Method.GET,uri,listener,errorListener);
         }
 
@@ -149,7 +187,43 @@ public class ElectricityAPI implements IElectricityAPI{
             }
 
             try {
-                callback.electricityRequestDone(new GPSCoord((float)lat.getDouble("value"), (float)lng.getDouble("value")));
+                callback.electricityRequestDone(new GPSCoord((float) lat.getDouble("value"), (float) lng.getDouble("value")));
+            }catch(JSONException e){
+                Log.e("JSONException","Error getting values from JSONObject-response");
+            }catch(NullPointerException e){
+                Log.e("NullPointer","Error reading JSONObjects");
+            }
+        }
+    }
+
+    private class NextStopParser implements Response.Listener<JSONArray>, Response.ErrorListener{
+
+        ElectricityHandler callback;
+
+        public NextStopParser(ElectricityHandler callback){
+            this.callback = callback;
+        }
+
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Log.e("ErrorResponse",error.toString());
+            callback.electricityRequestError(error.toString());
+        }
+
+        @Override
+        public void onResponse(JSONArray response) {
+            Log.i("Response",response.toString());
+            JSONObject lat = null;
+            JSONObject lng = null;
+            try {
+                lat = response.getJSONObject(1);
+                lng = response.getJSONObject(3);
+            }catch(JSONException e){
+                Log.e("JSONException","Error getting JSONObjects from response");
+            }
+
+            try {
+                callback.electricityRequestDone(new GPSCoord((float) lat.getDouble("value"), (float) lng.getDouble("value")));
             }catch(JSONException e){
                 Log.e("JSONException","Error getting values from JSONObject-response");
             }catch(NullPointerException e){
--- GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/api/IElectricityAPI.java
+++ GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/api/IElectricityAPI.java
@@ -1,10 +1,22 @@
 package com.teamteamname.gotogothenburg.api;
 
+import com.teamteamname.gotogothenburg.map.Bus;
+
 /**
  * Created by Olof on 25/09/2015.
  */
 public interface IElectricityAPI {
 
-    public void getBusLocation(String dgw, ElectricityHandler callback);
+    public void getBusLocation(Bus bus, ElectricityHandler callback);
+
+    public void getNextStop(Bus bus, ElectricityHandler callback);
+
+    public void getAmbientTemperature(Bus bus, ElectricityHandler callback);
+
+    public void getCabinTemperature(Bus bus, ElectricityHandler callback);
+
+    public void getStopPressed(Bus bus, ElectricityHandler callback);
+
+    public void getNbrOfWifiUsers(Bus bus, ElectricityHandler callback);
 
 }
