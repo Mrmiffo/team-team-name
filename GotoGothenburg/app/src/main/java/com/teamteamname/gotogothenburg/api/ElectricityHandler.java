ec2357df526093f32f9c82a30644334ad13de194
Olodus <olof.dusterdieck@gmail.com>
2015-09-28 13:55:24 +0200

    Added further response-methods in the Electricity response handler.

--- GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/api/ElectricityHandler.java
+++ GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/api/ElectricityHandler.java
@@ -7,7 +7,17 @@ import com.teamteamname.gotogothenburg.GPSCoord;
  * Created by Olof on 22/09/2015.
  */
 public interface ElectricityHandler {
-    public void electricityRequestDone(GPSCoord coord);
+    public void electricityGPSResponse(GPSCoord coord);
+
+    public void electricityNextStopResponse(String nextStop);
+
+    public void electricityAmbientTemperatureResponse(double temp);
+
+    public void electricityCabinTemperature(double temp);
+
+    public void electricityStopPressedResponse(boolean isPressed);
+
+    public void electricityWifiUsersResponse(int nbrOfUsers);
 
     public void electricityRequestError(String error);
 }
