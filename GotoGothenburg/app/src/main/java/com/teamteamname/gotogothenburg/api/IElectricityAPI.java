e7ca570a44acc08422787ac4814da71d2bdb8312
Olodus <olof.dusterdieck@gmail.com>
2015-09-29 09:33:39 +0200

    Added comments to the interface's methods.

--- GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/api/IElectricityAPI.java
+++ GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/api/IElectricityAPI.java
@@ -7,16 +7,28 @@ import com.teamteamname.gotogothenburg.map.Bus;
  */
 public interface IElectricityAPI {
 
+    // Returns the location of the given bus.
+    // Sensor updates each 5 sec.
     public void getBusLocation(Bus bus, ElectricityHandler callback);
 
+    // Returns the next stop for the given bus.
+    // Sensor updates each 10 sec.
     public void getNextStop(Bus bus, ElectricityHandler callback);
 
+    // Returns the temperature outside of the given bus.
+    // Sensor updates each 10 sec.
     public void getAmbientTemperature(Bus bus, ElectricityHandler callback);
 
+    // Returns the temperature inside the DRIVER'S cabin of the given bus.
+    // Sensor updates each 2,5 min.
     public void getCabinTemperature(Bus bus, ElectricityHandler callback);
 
+    // Returns whether the stop button is pressed for the given bus.
+    // Sensor updates each time someone presses the stop button.
     public void getStopPressed(Bus bus, ElectricityHandler callback);
 
-    public void getNbrOfWifiUsers(Bus bus, ElectricityHandler callback);
+    // Returns the total number of users connected to the Wifi on the given bus.
+    // Sensor updates each 12 sec.
+    public void  getNbrOfWifiUsers(Bus bus, ElectricityHandler callback);
 
 }
