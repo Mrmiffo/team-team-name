fda73e8a24819a9fb22d46028176e3275cb66f72
Kakan-IT14 <jesper.olsson191@gmail.com>
2015-09-30 10:41:23 +0200

    PointOfInterest now contains Files of relevant information

--- GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/PointOfInterest.java
+++ GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/PointOfInterest.java
@@ -2,43 +2,56 @@ package com.teamteamname.gotogothenburg;
 
 import java.io.File;
 
+import lombok.Getter;
+
 /**
+ * An enum representing a point of interest, complete with physical location, picture and information in both text and sound.
  * Created by kakan on 2015-09-28.
  */
 public enum PointOfInterest {
-    JOHANNEBERG_SCIENCE_PARK(new GPSCoord(57.684747f, 11.977967f)),
-    CHALMERS_CAMPUS_JOHANNEBERG(new GPSCoord(57.689167f, 11.973611f)),
-    LANDALA(new GPSCoord(57.693319f, 11.970892f)),
-    LANDSHÖVDINGEHUS(new GPSCoord()),
-    MILJONPROGRAMMET(new GPSCoord()),
-    POSEIDON(new GPSCoord(57.697206f, 11.979561f)),
-    GÖTEBORGS_KONSTMUSEUM(new GPSCoord(57.696389f, 11.980611f)),
-    STADSTEATERN(new GPSCoord(57.697444f, 11.980417f)),
-    GÖTEBORGS_KONSERTHUS(new GPSCoord(57.696694f, 11.978583f)),
-    KOPPARMÄRRA(new GPSCoord(57.704503f, 11.969636f)),
-    SALUHALLEN(new GPSCoord(57.703564f, 11.967867f)),
-    TURISTBYRÅN_KUNGSPORTSPLATSEN(new GPSCoord(57.704332f, 11.969843f)),
-    BRUNNSPARKEN(new GPSCoord(57.706875f, 11.969003f)),
-    SÅNINGSKVINNAN(new GPSCoord(57.706758f, 11.968575f)),
-    NORDSTAN(new GPSCoord(57.708803f, 11.968917f)),
-    TURISTBYRÅN_NORDSTAND(new GPSCoord(57.708444f, 11.969252f)),
-    ARKADEN(new GPSCoord(57.706389f, 11.969444f)),
-    NK(new GPSCoord(57.705695f, 11.968755f)),
-    DOMSTOLEN(new GPSCoord()),
-    LÄPPSTIFTET(new GPSCoord(57.713197f, 11.967481f)),
-    NEW_DEVELOPMENT(new GPSCoord()),
-    CHALMERS_CAMPUS_LINDHOLMEN(new GPSCoord()),
-    KUGGEN(new GPSCoord(57.706786f, 11.938908f)),
-    LINDHOLMEN_SCIENCE_PARK(new GPSCoord(57.707f, 11.94f)),
-    INOMHUSHÅLLPLATSEN(new GPSCoord(57.706911f, 11.93721f));
+    JOHANNEBERG_SCIENCE_PARK(new GPSCoord(57.684747f, 11.977967f), null, null, null),
 
-    private final GPSCoord location;
-    private File textGuide;
-    private File soundGuide;
-    private File picture;
+    CHALMERS_CAMPUS_JOHANNEBERG(new GPSCoord(57.689167f, 11.973611f),
+            new File(PointOfInterest.guideDir + "chalmersCampusJohannebergTest.txt"),
+            new File(PointOfInterest.guideDir +  "chalmersCampusJohannebergTest.mp3"),
+            new File(PointOfInterest.guideDir + "chalmersCampusJohannebergTest.jpg")),
 
-    PointOfInterest(final GPSCoord location) {
+    LANDALA(new GPSCoord(57.693319f, 11.970892f), null, null, null),
+    LANDSHÖVDINGEHUS(new GPSCoord(), null, null, null),                     //OBS
+    MILJONPROGRAMMET(new GPSCoord(), null, null, null),                     //OBS
+    POSEIDON(new GPSCoord(57.697206f, 11.979561f), null, null, null),
+    GÖTEBORGS_KONSTMUSEUM(new GPSCoord(57.696389f, 11.980611f), null, null, null),
+    STADSTEATERN(new GPSCoord(57.697444f, 11.980417f), null, null, null),
+    GÖTEBORGS_KONSERTHUS(new GPSCoord(57.696694f, 11.978583f), null, null, null),
+    KOPPARMÄRRA(new GPSCoord(57.704503f, 11.969636f), null, null, null),
+    SALUHALLEN(new GPSCoord(57.703564f, 11.967867f), null, null, null),
+    TURISTBYRÅN_KUNGSPORTSPLATSEN(new GPSCoord(57.704332f, 11.969843f), null, null, null),
+    BRUNNSPARKEN(new GPSCoord(57.706875f, 11.969003f), null, null, null),
+    SÅNINGSKVINNAN(new GPSCoord(57.706758f, 11.968575f), null, null, null),
+    NORDSTAN(new GPSCoord(57.708803f, 11.968917f), null, null, null),
+    TURISTBYRÅN_NORDSTAND(new GPSCoord(57.708444f, 11.969252f), null, null, null),
+    ARKADEN(new GPSCoord(57.706389f, 11.969444f), null, null, null),
+    NK(new GPSCoord(57.705695f, 11.968755f), null, null, null),
+    DOMSTOLEN(new GPSCoord(), null, null, null),                            //OBS
+    LÄPPSTIFTET(new GPSCoord(57.713197f, 11.967481f), null, null, null),
+    NEW_DEVELOPMENT(new GPSCoord(), null, null, null),                      //OBS
+    CHALMERS_CAMPUS_LINDHOLMEN(new GPSCoord(), null, null, null),           //OBS
+    KUGGEN(new GPSCoord(57.706786f, 11.938908f), null, null, null),
+    LINDHOLMEN_SCIENCE_PARK(new GPSCoord(57.707f, 11.94f), null, null, null),
+    INOMHUSHÅLLPLATSEN(new GPSCoord(57.706911f, 11.93721f), null, null, null);
+
+
+    private static String guideDir = "src/main/res/guide/";
+
+    @Getter private final GPSCoord location;
+    @Getter private File textGuide;
+    @Getter private File soundGuide;
+    @Getter private File picture;
+
+    PointOfInterest(final GPSCoord location, File textGuide, File soundGuide, File picture) {
         this.location = location;
-
+        this.textGuide = textGuide;
+        this.soundGuide = soundGuide;
+        this.picture = picture;
     }
 }
