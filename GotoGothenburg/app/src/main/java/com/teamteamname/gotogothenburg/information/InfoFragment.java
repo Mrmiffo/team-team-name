ec53347554cf7518b0be5b60e59659958181b885
Marvin043 <marvin.ztrubas@gmail.com>
2015-10-01 15:30:07 +0200

    Removed unused code from info fragment

--- GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/information/InfoFragment.java
+++ GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/information/InfoFragment.java
@@ -17,9 +17,6 @@ import com.teamteamname.gotogothenburg.R;
  * Created by Anton on 2015-09-21.
  */
 public class InfoFragment extends Fragment {
-
-    public static final String ARG_POS = "ARG_POS";
-
     public InfoFragment() {}
 
     public static InfoFragment newInstance(){
