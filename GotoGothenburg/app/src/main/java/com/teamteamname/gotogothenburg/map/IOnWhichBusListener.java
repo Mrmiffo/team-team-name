dd78e6add9059cdbe30cd66ad0b16a4bb5b9f428
Marvin043 <marvin.ztrubas@gmail.com>
2015-10-02 11:33:10 +0200

    Created callback interface for OnWhichBus class.

---
+++ GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/map/IOnWhichBusListener.java
@@ -0,0 +1,16 @@
+package com.teamteamname.gotogothenburg.map;
+
+/**
+ * A class that implements this interface may register to
+ * listen to the OnWhichBUsIdentifier class and will be called
+ * when the OnWhichBusIdentifier registers that the user is on a buss.
+ * Created by Anton on 2015-10-02.
+ */
+public interface IOnWhichBusListener {
+    /**
+     * This method will be called continually when the OnWhichBusIdentifier
+     * finds a bus that the user is on.
+     * @param busUserIsOn
+     */
+    void whichBussCallBack(Bus busUserIsOn);
+}
