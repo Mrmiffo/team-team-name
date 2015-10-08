642876afd6eb872fbb1003c1c22913e43e99bf0d
Drenata <andersson.patrick95@gmail.com>
2015-10-08 08:46:21 +0200

    Switch to MapFragment when "display resale points" button is pressed (relies on code from Matpan which doesn't exist in main branch when this commit was made)

--- GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/information/InfoFragment.java
+++ GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/information/InfoFragment.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.teamteamname.gotogothenburg.R;
+import com.teamteamname.gotogothenburg.activity.MainActivity;
 
 
 /**
@@ -27,7 +28,14 @@ public class InfoFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_info, container, false);
+        View view = inflater.inflate(R.layout.fragment_info, container, false);
+        view.findViewById(R.id.display_resale_points).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((MainActivity)getActivity()).changeTab(1);
+            }
+        });
+        return view;
     }
 
 
