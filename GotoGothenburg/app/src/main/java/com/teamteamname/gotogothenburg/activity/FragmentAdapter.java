35b37cbb7540cd3082401f12854ea605bd267bd0
Mattias Ahlstedt <mattias.5011@gmail.com>
2015-10-09 08:19:43 +0200

    implemented the callback method in searchbarListener for Vasttrafik trip
    Signed-off-by: Mattias Ahlstedt <mattias.5011@gmail.com>

--- GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/activity/FragmentAdapter.java
+++ GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/activity/FragmentAdapter.java
@@ -4,6 +4,8 @@ package com.teamteamname.gotogothenburg.activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.support.v13.app.FragmentPagerAdapter;
+import android.util.SparseArray;
+import android.view.ViewGroup;
 
 import com.teamteamname.gotogothenburg.destination.DestinationFragment;
 import com.teamteamname.gotogothenburg.information.InfoFragment;
@@ -21,10 +23,28 @@ public class FragmentAdapter extends FragmentPagerAdapter {
     //The names of the Fragments as displayed in the application.
     private final String[] TITLES = { "Destinations", "Map", "Info", "Settings" };
 
+    private SparseArray<Fragment> registeredFragments = new SparseArray<>();
+
     public FragmentAdapter(FragmentManager fm){
         super(fm);
     }
 
+    public Fragment getRegisteredFragment(int position) {
+        return registeredFragments.get(position);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        Fragment fragment = (Fragment) super.instantiateItem(container, position);
+        registeredFragments.put(position, fragment);
+        return fragment;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        registeredFragments.remove(position);
+        super.destroyItem(container, position, object);
+    }
 
     @Override
     public Fragment getItem(int position) {
