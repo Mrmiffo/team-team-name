c4ba1ee825e8961b95dbb2de3f098ef1ee7e7cfa
Marvin043 <marvin.ztrubas@gmail.com>
2015-10-09 08:41:09 +0200

    Modified Javadoc for destination to represent changes made.

--- GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/destination/Destination.java
+++ GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/destination/Destination.java
@@ -10,8 +10,10 @@ import lombok.Setter;
 //NOTE! JUnit tests has not been created for this class as all methods are auto-generated setters and getters with no logic. //Anton 2015-09-30
 
 /**
- * The basic destination class containing a location, name, create date and a boolean for if it's
- * visited. This class is created when the user select a place on the map to save into the list of
+ * The destination class containing a location, nameand a boolean for if it has been
+ * visited. This is an immutable final class in order to make sure that the save functionality
+ * remain intact. To modify a destination, please use the copy constructor.
+ * This class is created when the user select a place on the map to save into the list of
  * destinations to visit. This class is not immutable as the user may want to change information
  * such as the name.
  * Created by Anton on 2015-09-30.
