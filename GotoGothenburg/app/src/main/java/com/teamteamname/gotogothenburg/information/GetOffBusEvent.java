c19b581452e494f9788a47a4748ab4292f3b5daa
Olodus <olof.dusterdieck@gmail.com>
2015-10-13 08:23:55 +0200

    Added classes for notifying the user during a trip.

---
+++ GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/information/GetOffBusEvent.java
@@ -0,0 +1,11 @@
+package com.teamteamname.gotogothenburg.information;
+
+/**
+ * Created by Olof on 12/10/2015.
+ */
+public class GetOffBusEvent implements Runnable {
+    @Override
+    public void run() {
+
+    }
+}
