85bd2d49aa58472297831f9a64250916085ead47
Marvin043 <marvin.ztrubas@gmail.com>
2015-10-14 10:42:03 +0200

    Added info of tickets and how to travel to the Info tab.

--- GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/information/InfoFragment.java
+++ GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/information/InfoFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.teamteamname.gotogothenburg.R;
 import com.teamteamname.gotogothenburg.activity.MainActivity;
@@ -14,6 +15,7 @@ import com.teamteamname.gotogothenburg.activity.MainActivity;
  * Created by Anton on 2015-09-21.
  */
 public class InfoFragment extends Fragment {
+    private TextView ticketTitle, ticketText, howToTravelTitle, howToTravelText;
 
     public InfoFragment() {}
 
@@ -28,7 +30,20 @@ public class InfoFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_info, container, false);
+        View toReturn = inflater.inflate(R.layout.fragment_info, container, false);
+        //Fetch all text fields.
+        ticketTitle = (TextView) toReturn.findViewById(R.id.ticket_title);
+        ticketText = (TextView) toReturn.findViewById(R.id.ticket_text);
+        howToTravelTitle = (TextView) toReturn.findViewById(R.id.how_to_travel_title);
+        howToTravelText = (TextView) toReturn.findViewById(R.id.how_to_travel_text);
+
+        //Add text from the @string
+        ticketTitle.setText(R.string.info_frag_ticket_title);
+        ticketText.setText(R.string.info_frag_ticket_text);
+        howToTravelTitle.setText(R.string.info_frag_how_to_travel_title);
+        howToTravelText.setText(R.string.info_frag_how_to_travel_text);
+
+        return toReturn;
     }
 
 
