161c1d7f3104ff3153418ec8152df0d21d59469e
Olodus <olof.dusterdieck@gmail.com>
2015-10-14 09:00:30 +0200

    Made the RouteNotifier able to create events and post them at specific times.

--- GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/information/RouteNotifier.java
+++ GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/information/RouteNotifier.java
@@ -1,11 +1,31 @@
 package com.teamteamname.gotogothenburg.information;
 
+import android.content.Context;
 import android.os.Handler;
+import android.os.Looper;
 
 /**
  * Created by Olof on 12/10/2015.
  */
 public class RouteNotifier extends Handler {
 
+    private Context context;
 
+    public RouteNotifier(Context context){
+        super(Looper.getMainLooper());
+        this.context = context;
+    }
+
+    public RouteNotifier(Context context, Looper looper){
+        super(looper);
+        this.context = context;
+    }
+
+    public void addCatchBusEvent(long timestamp, int busLine, String stopName, String stopPosition){
+        this.postAtTime(new CatchingBusEvent(context,busLine,stopName,stopPosition),timestamp);
+    }
+
+    public void addGetOffBusEvent(long timestamp, int busLine, String stopName, String stopPosition){
+        this.postAtTime(new GetOffBusEvent(context,busLine,stopName,stopPosition),timestamp);
+    }
 }
