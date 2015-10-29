b36f5e721ecf5b6c51db2d80dcaa8a59f74de379
Kakan-IT14 <jesper.olsson191@gmail.com>
2015-10-29 08:43:59 +0100

    removed out-commentated code in Stops

--- GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/api/Stops.java
+++ GotoGothenburg/app/src/main/java/com/teamteamname/gotogothenburg/api/Stops.java
@@ -1,41 +1,42 @@
 package com.teamteamname.gotogothenburg.api;
 
 /**
- * An enum containing all valid stops for buses, tram etc.
+ * An enum containing all valid stops for buses, tram etc. Each stop contains zero or more Points of Interest
  * Created by kakan on 2015-09-25.
  */
 public enum Stops {
     //Note that only stops located along line 55 are included in the prototype.
-    CHALMERS_TVÄRGATA(
+    CHALMERS_TVÄRGATA(),
 
-    ),
+    ÅLANDSGATAN(),
 
-    ÅLANDSGATAN(
-
-    ),
     SVEN_HULTINS_PLATS(
             PointOfInterest.JOHANNEBERG_SCIENCE_PARK
     ),
+
     CHALMERSPLATSEN(
             PointOfInterest.CHALMERS_CAMPUS_JOHANNEBERG
     ),
+
     KAPELLPLATSEN(
             PointOfInterest.LANDALA
-//            PointOfInterest.LANDSHÖVDINGEHUS,
-//            PointOfInterest.MILJONPROGRAMMET
     ),
+
     GÖTAPLATSEN(
             PointOfInterest.POSEIDON,
             PointOfInterest.GÖTEBORGS_KONSTMUSEUM,
             PointOfInterest.STADSTEATERN,
             PointOfInterest.GÖTEBORGS_KONSERTHUS
     ),
+
     VALAND(),
+
     KUNGSPORTSPLATSEN(
             PointOfInterest.KOPPARMÄRRA,
             PointOfInterest.SALUHALLEN,
             PointOfInterest.TURISTBYRÅN_KUNGSPORTSPLATSEN
     ),
+
     BRUNNSPARKEN(
             PointOfInterest.BRUNNSPARKEN,
             PointOfInterest.SÅNINGSKVINNAN,
@@ -43,16 +44,20 @@ public enum Stops {
             PointOfInterest.TURISTBYRÅN_NORDSTAND,
             PointOfInterest.ARKADEN,
             PointOfInterest.NK
-//            PointOfInterest.DOMSTOLEN
     ),
+
     LILLA_BOMMEN(
             PointOfInterest.LÄPPSTIFTET
     ),
+
     FRIHAMNSPORTEN(
             PointOfInterest.ÄLVSTADEN_FRIHAMNEN
     ),
+
     PUMPGATAN(),
+
     REGNBÅGSGATAN(),
+
     LINDHOLMEN(
             PointOfInterest.CHALMERS_CAMPUS_LINDHOLMEN,
             PointOfInterest.KUGGEN,
@@ -61,12 +66,12 @@ public enum Stops {
     TEKNIKGATAN(
             PointOfInterest.INOMHUSHÅLLPLATSEN
     ),
+
     LINDHOLMSPLATSEN(),
+
     DEFAULT();
 
-
     private PointOfInterest[] pointOfInterests;
-
     Stops(PointOfInterest... pointOfInterests) {
         this.pointOfInterests = pointOfInterests.clone();
     }
